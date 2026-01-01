bcdc8e29310df8c09cb127913e1ec3cbd9c782c9
AadiCode04 <aadigithub61@gmail.com>
2026-01-01 12:08:47 +0530

    Added DivideAndConquer folder

---
+++ DivideAndConquer/RotatedArray.java
@@ -0,0 +1,54 @@
+package DivideAndConquer;
+
+public class RotatedArray {
+
+    public static int search(int arr[] ,int target, int si , int ei){
+         
+        //base case 
+
+        if(si > ei){
+            return -1;
+        }
+
+
+        // work
+        int mid = si + (ei-si)/2;
+        // case  = found
+        if(arr[mid] == target){
+            return mid ;
+        }
+
+        //case 1 : mid on Line 1 
+        if (arr[si] <= arr[mid] ) {
+
+            // case a : left
+            if( arr[si] <= target && target<= arr[mid]){
+                 return search(arr, target, si, mid);
+                   
+            }else{
+                // case b : right 
+                return search(arr, target, mid+1, ei);
+            }
+        }
+
+        //case 2 : mid on line 2
+        else{
+            // case c : right 
+            if (arr[mid]  <= target && target <= arr[ei]) {
+                return search(arr, target, mid +1, ei);
+            }else{
+                //case d : left
+                return search(arr, target, si, mid -1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0 ; // output -> 4
+        int TargetIndex = search(arr, target , 0, arr.length-1);
+        System.out.println(TargetIndex);
+    }
+}
