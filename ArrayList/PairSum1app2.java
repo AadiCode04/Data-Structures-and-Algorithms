a348ea64e45c2f31be83475459d573e6da994a0f
AadiCode04 <aadigithub61@gmail.com>
2026-01-18 12:18:22 +0530

    ArrayList added

---
+++ ArrayList/PairSum1app2.java
@@ -0,0 +1,44 @@
+// 2 pointer Approach
+// time Complexity O(n)
+// Sorted arrayList
+
+import java.util.ArrayList;
+
+public class PairSum1app2 {
+
+    public static boolean sumpair(ArrayList<Integer> list, int target) {
+
+        int lp = 0;
+        int rp = list.size() - 1;
+
+        while (lp != rp) {
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+
+            if (list.get(lp) + list.get(rp) < target) {
+                lp++;
+
+            } else {
+                rp--;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+
+        System.out.println(sumpair(list, target));
+    }
+}
